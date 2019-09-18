3558cf609c52b1013becc48191aa06bdd0caa65d
MarcDonovan1 <39577801+MarcDonovan1@users.noreply.github.com>
2019-09-18 10:57:59 -0400

    Update udpSender.py

--- udpSender.py
+++ udpSender.py
@@ -11,16 +11,19 @@ port = int(textport)
 server_address = (host, port)
 
 while 1:
-    print ("Enter data to transmit: ENTER to quit")
-    data = sys.stdin.readline().strip()
-    if not len(data):
-        break
+   # print ("Enter data to transmit: ENTER to quit")
+   # data = sys.stdin.readline().strip()
+    #if not len(data):
+    #    break
 #    s.sendall(data.encode('utf-8')    
-    s.sendto(data.encode('utf-8'), server_address)
+   # s.sendto(data.encode('utf-8'), server_address)
     
-    for (int i = 1; i<=number;i+=1):
-        new_message= "Number"+(str)i
-        s.endto(new_message.encode('utf-8'),server_address)
-
+    int i = 1
+    new_message= "Number"+(str)i
+    s.endto(new_message.encode('utf-8'),server_address)
+    
+    if (i>=number):
+        break
+    i++
 s.shutdown(1)
 
